8e8a33a5806d071c601ed710fb6e7894fff3ddeb
PitchforkAssistant <pitchforkassistant@gmail.com>
2024-03-06 14:08:34 +0200

    don't store new actions on old posts

--- src/handlers/modAction.ts
+++ src/handlers/modAction.ts
@@ -1,21 +1,28 @@
 import {ModAction} from "@devvit/protos";
 import {TriggerContext} from "@devvit/public-api";
 import {trackActionTime, untrackActionTime} from "../helpers/redisHelpers.js";
+import {DEFAULTS, KEYS} from "../constants.js";
 
 export async function onModAction (event: ModAction, context: TriggerContext) {
     // We're only interested in link removals
     if (event.action !== "removelink" && event.action !== "spamlink" && event.action !== "approvelink") {
         return;
     }
+    console.log(`processing ${event.action} for ${event.targetPost?.id}`);
 
     const postId = event.targetPost?.id;
     const actionedAt = event.actionedAt;
-    if (!postId || !actionedAt) {
-        console.error(`Missing postId (${postId}) or actionedAt (${actionedAt?.toString()}) in onModAction`);
+    const createdAt = event.targetPost?.createdAt;
+    if (!postId || !actionedAt || !createdAt) {
+        console.error(`Missing postId (${postId}), actionedAt (${actionedAt?.getTime()}), or createdAt (${createdAt}) in onModAction`);
         return;
     }
 
-    console.log(`processing ${event.action} for ${postId}`);
+    const quotaPeriod = await context.settings.get<number>(KEYS.QUOTA_PERIOD) ?? DEFAULTS.MAX_QUOTA_PERIOD;
+    if (actionedAt.getTime() - createdAt > quotaPeriod * 1000 * 60 * 60) {
+        console.log(`Ignoring ${event.action} for ${postId} as it's too old`);
+        return;
+    }
 
     if (event.action === "approvelink") {
         await untrackActionTime(context.redis, "remove", postId);
--- src/handlers/postDelete.ts
+++ src/handlers/postDelete.ts
@@ -1,6 +1,7 @@
 import {EventSource, PostDelete} from "@devvit/protos";
 import {TriggerContext} from "@devvit/public-api";
 import {trackActionTime} from "../helpers/redisHelpers.js";
+import {DEFAULTS, KEYS} from "../constants.js";
 
 export async function onPostDelete (event: PostDelete, context: TriggerContext) {
     // The PostDelete event name is a bit misleading,
@@ -11,8 +12,15 @@ export async function onPostDelete (event: PostDelete, context: TriggerContext) 
 
     console.log(`processing user deletion of ${event.postId}`);
     const deletedAt = event.deletedAt;
-    if (!deletedAt) {
-        console.error(`Missing deletedAt (${deletedAt}) in onPostDelete`);
+    const createdAt = event.createdAt;
+    if (!deletedAt || !createdAt) {
+        console.error(`Missing deletedAt (${deletedAt?.toString()}) or createdAt (${createdAt?.toString()}) in onPostDelete`);
+        return;
+    }
+
+    const quotaPeriod = await context.settings.get<number>(KEYS.QUOTA_PERIOD) ?? DEFAULTS.MAX_QUOTA_PERIOD;
+    if (deletedAt.getTime() - createdAt.getTime() > quotaPeriod * 1000 * 60 * 60) {
+        console.log(`Ignoring deletion for ${event.postId} as it's too old`);
         return;
     }
 
