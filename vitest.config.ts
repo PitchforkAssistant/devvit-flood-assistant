27e5b00d7d66987a2fe4f6be0f074082f7c53405
Trent Bartlem <TrentBartlem@users.noreply.github.com>
2025-03-31 00:20:19 +1000

    Added github action pipeline (#14)
    * add testing pipeline
    
    * uncomment example test
    
    * newer versions of eslint reject --ext if config file exists
    
    * added lint:fix
    
    * configure github integration on failing test cases
    
    * auto-commit any fixes needed
    
    * typo
    
    * added write contents permission
    
    * Apply automatic changes
    
    * adding audit fix
    
    * use --force to fix breaking changes
    
    * Bump vitest from 2.1.2 to 2.1.9
    
    Bumps [vitest](https://github.com/vitest-dev/vitest/tree/HEAD/packages/vitest) from 2.1.2 to 2.1.9.
    - [Release notes](https://github.com/vitest-dev/vitest/releases)
    - [Commits](https://github.com/vitest-dev/vitest/commits/v2.1.9/packages/vitest)
    
    ---
    updated-dependencies:
    - dependency-name: vitest
      dependency-type: direct:development
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Apply automatic changes
    
    * remove audit force
    
    * fix typo
    
    * Bump vite from 6.2.2 to 6.2.3 in the npm_and_yarn group
    
    Bumps the npm_and_yarn group with 1 update: [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite).
    
    
    Updates `vite` from 6.2.2 to 6.2.3
    - [Release notes](https://github.com/vitejs/vite/releases)
    - [Changelog](https://github.com/vitejs/vite/blob/v6.2.3/packages/vite/CHANGELOG.md)
    - [Commits](https://github.com/vitejs/vite/commits/v6.2.3/packages/vite)
    
    ---
    updated-dependencies:
    - dependency-name: vite
      dependency-type: indirect
      dependency-group: npm_and_yarn
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: TrentBartlem <171650+TrentBartlem@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,8 @@
 import {defineConfig} from "vitest/config";
 
-export default defineConfig({test: {globals: true}});
+export default defineConfig({
+    test: {
+        reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["dot"],
+        globals: true,
+    },
+});
